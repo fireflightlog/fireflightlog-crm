8af37d3c3a9ca9ece97ddf8a2078efb65351fd44
Nicholas Tilbury <ntilbury@outlook.com>
2025-07-07 14:45:29 +0800

    test

---
+++ sync-users.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+const {
+  AIRTABLE_TOKEN,
+  AIRTABLE_BASE_ID,
+  AIRTABLE_USERS_TABLE,
+  SUPABASE_URL,
+  SUPABASE_SERVICE_ROLE_KEY,
+  SUPABASE_USERS_TABLE
+} = process.env;
+
+// [keep rest of script the same as before]
